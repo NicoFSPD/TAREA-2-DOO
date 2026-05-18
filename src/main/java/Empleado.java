00aa94e68416a53749347a6e319e5c4d21e6b223
NicoFSPD <linegarcina@gmail.com>
2026-05-18 01:17:34 -0400

    feat: Primera implementación a clase Empleado

---
+++ src/main/java/Empleado.java
@@ -0,0 +1,12 @@
+public class Empleado implements Invitable{
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+
+
+    @Override
+    public void invitar() {
+        
+    }
+}
