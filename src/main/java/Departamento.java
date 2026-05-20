e1fdc91da5fe96b2d741cd39a43b6705db5ac4e2
eduearm <eduearm29@gmail.com>
2026-05-19 23:42:00 -0400

    feat: Impleentacion de todos los atributos de la clase Departamento, al igual que los metodos de obtencion de cantidad de empleados, agregar empleados y bconstructos donde se declara el arreglo de personas pertenecientes a este. Tambien se implemento el metodo abstracto de invitacion a todo un departamento.

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -1,7 +1,34 @@
-public class Departamento {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Departamento implements Invitable {
     private String nombre;
+    private List<Empleado> empleados;   //lista de los empleados que debeerian pertenecer al departamento en cuestion
+
+    public Departamento(String nombre){
+        this.nombre= nombre;
+        this.empleados = new ArrayList<>();
+    }
+
+    public void agregarEmpleado(Empleado empleado){
+        if (empleado != null && !empleados.contains(empleado)){
+            empleados.add(empleado);
+        }
+    }
 
     public int obtenerCantidadEmpleados(){
-        return cantidadEmpleados;
+        return empleados.size();
     }
+
+    @Override
+    public void invitar(){
+        System.out.println("Invitacion a todos los empleados del departamento " + nombre + " enviada");
+        for(Empleado emp: empleados){
+            emp.invitar();      //el ciclo invita a todos los de la lista del departamento
+        }
+    }
+
+
+
+
 }
