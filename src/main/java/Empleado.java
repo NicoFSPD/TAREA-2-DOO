2f46311ab2d13ad7f59bad63661e429a5d5db431
eduearm <eduearm29@gmail.com>
2026-05-19 23:20:30 -0400

    feat: Implementacion de los getters y setters de cada atributo, más sus respectvo toString

--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -18,6 +18,55 @@ public class Empleado implements Invitable{
     public void invitar() {
         System.out.println("Invitacion enviada a " + correo);
     }
-    
+
+    //----------- GETTERS Y SETTERS -------------
+
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getApellidos(){
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(){
+        this.nombre = nombre;
+    }
+
+    public String getCorreo(){
+        return correo;
+    }
+
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+
+    public Departamento getDepartamento(){
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento){
+        this.departamento = departamento;
+    }
+
+
+    @Override
+    public String toString(){
+        return "Empleado {" + "id= " + id + "/" + " nombre completo= " + nombre + " " + apellidos + "/" + "correo= " + correo + "/" + "departamento" +
+                (departamento!= null ? departamento.getNombre() : "ninguno");
+    }
 
 }
