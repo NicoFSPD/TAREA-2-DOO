47d72e795feef08fac04e903ff842627f873e542
eduearm <eduearm29@gmail.com>
2026-05-19 22:46:58 -0400

    feat: Implementacion de los atributs a la clase Empleado con su respectivo constructor y el emetodo abstracto.

--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -3,10 +3,21 @@ public class Empleado implements Invitable{
     private String nombre;
     private String apellidos;
     private String correo;
+    private String Departamento departamento;
+
+    public Empleado(String id, String apellidos, String nombre, String correo, Departamento departamento){
+        this.id = id;
+        this.apellidos = apellidos;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.departamento = departamento;
+    }
 
 
     @Override
     public void invitar() {
-        
+        System.out.println("Invitacion enviada a " + correo);
     }
+    
+
 }
