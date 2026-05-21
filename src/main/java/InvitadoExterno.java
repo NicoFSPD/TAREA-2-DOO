a1adfc254e5ad2bc2d349482a26ffe65d1cf289b
Daniel Cristobal Patricio Lopez Ramirez <lopezramirezdaniel946@gmail.com>
2026-05-21 11:53:19 -0400

    feat: Implementacion de funcionalidad/clase InvitadoExterno

---
+++ src/main/java/InvitadoExterno.java
@@ -0,0 +1,46 @@
+public class InvitadoExterno implements Invitable {
+
+    private String nombre;
+    private String apellidos;
+    private String correo;
+
+    public InvitadoExterno(String apellidos, String nombre, String correo){
+        this.apellidos = apellidos;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    @Override
+    public void invitar() {
+        System.out.println("Invitacion enviada a " + correo);
+    }
+
+    public String getApellidos(){
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getCorreo(){
+        return correo;
+    }
+
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString(){
+        return "InvitadoExterno {" + "nombre completo= " + nombre + " " + apellidos + "/" + "correo= " + correo + "}";
+    }
+}
