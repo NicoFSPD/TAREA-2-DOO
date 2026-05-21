59f410f1b797d8133d30c918a6b6e42900b72a3d
NicoFSPD <linegarcina@gmail.com>
2026-05-21 14:19:06 -0400

    docs: Agregar documentación a Invitacion

--- src/main/java/Invitacion.java
+++ src/main/java/Invitacion.java
@@ -1,27 +1,31 @@
 import java.time.Instant;
-
+/**Clase para las invitaciones
+ * @author Nicolás Silva
+ * @author Daniel Ramirez
+ * @version 1.0, 20 de mayo de 2026
+ * */
 public class Invitacion {
-
+    //Implementación interpretada, adicional a lo ya entregado en el UML
     private Instant hora;
     private Invitable invitado;
-
+    /**Constructor de la clase
+     * @param hora
+     * @param invitado */
     public Invitacion(Instant hora, Invitable invitado) {
         this.hora = hora;
         this.invitado = invitado;
     }
-
+    //Getter y Setter para la hora
     public Instant getHora() {
         return hora;
     }
-
     public void setHora(Instant hora) {
         this.hora = hora;
     }
-
+    //Getter y Setter para el Invitado
     public Invitable getInvitado() {
         return invitado;
     }
-
     public void setInvitado(Invitable invitado) {
         this.invitado = invitado;
     }
