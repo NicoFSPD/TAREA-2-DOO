f03138340fb0e89b2f620f63dae18bc2dcd3e3db
eduearm <eduearm29@gmail.com>
2026-05-21 22:10:07 -0400

    docs: documentacion completa de la clase Invitable.java

--- src/main/java/Invitable.java
+++ src/main/java/Invitable.java
@@ -1,3 +1,21 @@
+/**
+ * Interfaz que define el contrato de comportamiento para cualquier entidad
+ * que pueda ser convocada o citada a una reunión dentro del sistema.
+ * <p>
+ * Mediante esta interfaz, se abstracta el concepto de "participante" o "asociado",
+ * permitiendo que ,con el uso de polimorfismo, clases con codigo distinto (como Departamento
+ * o empleado) puedan ser manejadas de forma distinta.
+ * </p>
+ * * @author Eduardo Riveros Medina
+ * @version 1.0
+ */
 public interface Invitable {
+    /**
+     * Notifica la invitación..
+     * <p>
+     * Las clases que implementen este metodo deben definen su propia logica
+     * específica de envío.
+     * </p>
+     */
     public void invitar();
 }
