33a942801211539562b0b1dc62d527f4892ac0c0
NicoFSPD <linegarcina@gmail.com>
2026-05-22 04:58:31 -0400

    docs: Actualización de la documentación de Invitable

--- src/main/java/Invitable.java
+++ src/main/java/Invitable.java
@@ -1,5 +1,12 @@
+/**Interface para la invitación
+ * @author Nicolás Silva
+ * @version 2.0 - 22 de mayo de 2026*/
+
 public interface Invitable {
+    //Funcion invitar() sacada de lo visto en el UML
     public void invitar();
-    public String getNombre(); //funcionalidad para obtener nombres de participantes
+
+    //funcionalidad para obtener nombre/apellido de cada participante
+    public String getNombre();
     public String getApellidos();
 }
