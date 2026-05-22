1b7e1d3693cb7be5e1c21c5869036aa2cf0bd6f8
NicoFSPD <linegarcina@gmail.com>
2026-05-22 05:38:53 -0400

    Merge: Archivo desde premain (Version definitiva de clase Invitacion)

--- src/main/java/Invitacion.java
+++ src/main/java/Invitacion.java
@@ -1,5 +1,34 @@
 import java.time.Instant;
+/**Clase para las invitaciones
+ * @author Nicolás Silva
+ * @author Daniel Ramirez
+ * @version 1.0, 20 de mayo de 2026
+ * */
+public class Invitacion {
 
-public class Invitacion{
-    Instant hora;
+    private Instant hora;
+    private Invitable invitado;
+    /**Constructor de la clase
+     * @param hora
+     * @param invitado */
+    public Invitacion(Instant hora, Invitable invitado) {
+        this.hora = hora;
+        this.invitado = invitado;
+    }
+    //GETTER/SETTER PARA LA HORA
+
+    public Instant getHora() {
+        return hora;
+    }
+    public void setHora(Instant hora) {
+        this.hora = hora;
+    }
+
+    //GETTER/SETTER DE INVITADO
+    public Invitable getInvitado() {
+        return invitado;
+    }
+    public void setInvitado(Invitable invitado) {
+        this.invitado = invitado;
+    }
 }
